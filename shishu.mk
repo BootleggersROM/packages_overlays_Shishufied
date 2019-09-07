ebf7f3559ff22f5d240632a223a21e81b112ceb7
El Dainosor <eldainosor@gmail.com>
2019-09-07 23:32:05 +0000

    overlays: Add widebois support for default wallpaper on themes
    This is because it's annoying to see those big screens with off-centered walls.

--- shishu.mk
+++ shishu.mk
@@ -121,6 +121,12 @@ PRODUCT_PACKAGES += \
     ShishuProtostarThemeSystemUIOverlay \
     ShishuProtostarThemeWellbeingOverlay
 
+# Wallpaper overlay selection for widescreen devices.
+ifeq ($(BOOTLEGGERS_NOTCHED),true)
+    DEVICE_PACKAGE_OVERLAYS += \
+        packages/overlays/Shishufied/Themes/overlay/widebois
+endif
+
 #Quicksettings Tiles icons
 PRODUCT_PACKAGES += \
 	QSTileSquircle \
