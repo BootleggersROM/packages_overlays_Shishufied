e16a93b36e7d057d0cbdb8d8a6fee04059526192
El Dainosor <eldainosor@gmail.com>
2020-06-17 23:02:29 -0300

    themes: Bring back Shishu Illusions
    Change-Id: I9dff033bd42e5f83b3d6f606223fdf856add2927

--- Themes/ShishuIllusionsTheme/android/Android.mk
+++ Themes/ShishuIllusionsTheme/android/Android.mk
@@ -3,7 +3,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_PACKAGE_NAME := ShishuIllusionsThemeAndroidOverlay
+LOCAL_PACKAGE_NAME := ShishuThemeAndroidOverlay
 LOCAL_SDK_VERSION := current
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := false
--- Themes/ShishuIllusionsTheme/systemui/Android.mk
+++ Themes/ShishuIllusionsTheme/systemui/Android.mk
@@ -3,7 +3,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_PACKAGE_NAME := ShishuIllusionsThemeSystemUIOverlay
+LOCAL_PACKAGE_NAME := ShishuThemeSystemUIOverlay
 LOCAL_SDK_VERSION := current
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := false
