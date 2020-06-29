3bca05955012c229268abd6adf1f2f81fefec204
El Dainosor <eldainosor@gmail.com>
2020-06-29 08:58:57 -0400

    stub: properly add wallpapers
    We were using an overlay when we could do a check on the makefile to add the right resource folder, instead of just making a folder mess
    
    Change-Id: I5541b13a7568ae65d62e773b3157d346e39ffb81

--- Main/Android.mk
+++ Main/Android.mk
@@ -3,6 +3,15 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
 
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+ifeq ($(BOOTLEGGERS_NOTCHED),true)
+LOCAL_RESOURCE_DIR += $(LOCAL_PATH)/res-wallpaper-long
+else
+LOCAL_RESOURCE_DIR += $(LOCAL_PATH)/res-wallpaper
+endif
+
+LOCAL_AAPT_FLAGS := --auto-add-overlay
+
 LOCAL_PACKAGE_NAME := Shishufied
 LOCAL_SDK_VERSION := current
 LOCAL_CERTIFICATE := platform
